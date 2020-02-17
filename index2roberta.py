8c511824452cc6a535829184ef76b6c2d952ba53
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-02-17 16:24:44 -0500

    unfinished with pdb

--- index2roberta.py
+++ index2roberta.py
@@ -2,12 +2,16 @@ import os, sys
 import pickle
 import torch
 from transformers import *
+from collections import defaultdict
 tokenizer_class, weights, model_class= XLMRobertaTokenizer, 'xlm-roberta-base', XLMRobertaModel
 tokenizer = tokenizer_class.from_pretrained(weights)
 model = model_class.from_pretrained(weights)
 
 print("Reading Tensors")
 vector_dict = {}
+batch = defaultdict(list)
+batch_ids = defaultdict(list)
+
 for line in open(os.path.abspath(sys.argv[1]), 'r', encoding="utf-8"):
 	columns = line.strip().split("\t")
 	entries = columns[1:]
@@ -19,6 +23,8 @@ for line in open(os.path.abspath(sys.argv[1]), 'r', encoding="utf-8"):
 		tokens = tokens[:512]
 	tok_ids = tokenizer.convert_tokens_to_ids(tokens)
 	tok_tensors = torch.tensor([tok_ids])
+	batch[tok_tensors.size()[-1]].append(tok_tensors)
+	batch[tok_tensors.size()[-1]].append(int(columns[0]))
 
 	with torch.no_grad():
 		class_hidden_state = model(tok_tensors)[0]
@@ -28,6 +34,12 @@ for line in open(os.path.abspath(sys.argv[1]), 'r', encoding="utf-8"):
 		sys.stdout.write(str(len(vector_dict))+"...")
 
 sys.stdout.write(str(len(vector_dict))+"\n")
+
+for length in batch.keys():
+	print("hi")
+	import pdb
+	pdb.set_trace()
+
 
 sorted_tensors = sorted(vector_dict.values(), key = lambda kv:(kv[1], kv[0]))
 
