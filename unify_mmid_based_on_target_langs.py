2dc9c4606ea08757e402ebe0859e08cb82cfa935
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-02-20 15:29:37 -0500

    some changes to mmid_explore

--- unify_mmid_based_on_target_langs.py
+++ unify_mmid_based_on_target_langs.py
@@ -2,5 +2,9 @@ import glob, os,sys
 
 english_prefix = os.path.abspath(sys.argv[1])
 
-for file in glob.glob(english_prefix + "*"):
-    print(file)
+for folder in glob.glob(english_prefix + "*"):
+    print(folder)
+    for subdir in os.listdir(folder):
+        word = open(os.path.join(folder, subdir, "word.txt"),'r').read().strip()
+        print(subdir, word)
+
