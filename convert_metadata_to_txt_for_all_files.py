ca4d670b8c44ce3686a21af572706bfa45fd0d76
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-02-21 16:51:31 -0500

    fixed_wrong_paths_for_...

--- convert_metadata_to_txt_for_all_files.py
+++ convert_metadata_to_txt_for_all_files.py
@@ -9,15 +9,15 @@ index_content = []
 num_processed = 0
 for subdir in os.listdir(input_folder):
     subdir_path = os.path.join(input_folder, subdir)
-    num_processed += 1
-    if num_processed%100==0:
-        print(num_processed)
+
     for json_name in glob.glob(subdir_path + "/*metadata.json"):
-        print(json_name)
+        num_processed += 1
+        if num_processed % 1000 == 0:
+            print(num_processed)
         file_prefix = json_name[:json_name.rfind("-")+1]
         file_prefix_path = os.path.join(subdir_path, file_prefix)
         file_prefix = ntpath.basename(file_prefix)
-        directory_path = ntpath.basename(os.path.split(file_prefix_path)[0])
+        directory_path = ntpath.basename(input_folder)
         with open(json_name, 'r') as json_file:
             json_data = json.load(json_file)
             word = open(json_name[:-13]+"word.txt","r").read().strip()
@@ -34,7 +34,7 @@ for subdir in os.listdir(input_folder):
                     original_filename = json_data[key]["original_filename"]
                 except:
                     original_filename = "-"
-                correspond_file_path = os.path.join(directory_path , file_prefix+ key + ".jpg")
+                correspond_file_path = directory_path +"/"+subdir_path +"/"+ key + ".jpg"
                 index_content.append("\t".join([word, correspond_file_path, page_url, image_link]))
 
 with open(os.path.abspath(sys.argv[2]), "w") as writer:
--- find_matching_passage_for_images.py
+++ find_matching_passage_for_images.py
@@ -1,22 +1,25 @@
-import os,sys, pickle
+import os,sys, pickle,json
 
 with open(os.path.abspath(sys.argv[1]), "rb") as fin:
     target_uri_dict = pickle.load(fin)
 
 counter = 0
-with open(os.path.abspath(sys.argv[3]), "w") as writer:
+with open(os.path.abspath(sys.argv[3]), "w", encoding="utf-8") as writer:
+    image_text_dict = {}
     for line in open(os.path.abspath(sys.argv[2]), "r"):
         word, correspond_file_path, page_url, image_link = line.split("\t")
         counter+=1
-        if counter %1000==0:
+        if counter %100000==0:
             print(counter)
         if page_url in target_uri_dict:
-            texts = page_url.split("\n")
+            texts = target_uri_dict[page_url].split("\n")
             contains_text = []
             for text in texts:
                 if word.lower() in text.lower():
                     contains_text.append(text)
             if len(contains_text)>0:
-                contains_text = " ".join(contains_text)
-                writer.write(line+"\t"+contains_text+"\n")
+                contains_text_dict = {i:text for i, text in enumerate(contains_text)}
+                image_text_dict[correspond_file_path] = {"word":word, "text":contains_text_dict, "url": page_url, "image":image_link}
+    json.dump(image_text_dict, writer, ensure_ascii=False)
+
 print("done!")
