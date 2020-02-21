bc94390857932b2fc742fd3197ce1f67a7dc37c1
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-02-21 17:02:11 -0500

    added root folder to it

--- convert_metadata_to_txt_for_all_files.py
+++ convert_metadata_to_txt_for_all_files.py
@@ -1,12 +1,20 @@
-import os,sys
 import glob
 import json
 import ntpath
+import os
+import sys
 
 # root directory for the image data that has subdirectories.
 input_folder = os.path.abspath(sys.argv[1])
 index_content = []
 num_processed = 0
+
+base_input_dir = input_folder
+if base_input_dir.endswith("/"):
+    base_input_dir = base_input_dir[:-1]
+if "/" in base_input_dir:
+    base_input_dir = base_input_dir[base_input_dir.rfind("/") + 1:]
+
 for subdir in os.listdir(input_folder):
     subdir_path = os.path.join(input_folder, subdir)
 
@@ -14,18 +22,18 @@ for subdir in os.listdir(input_folder):
         num_processed += 1
         if num_processed % 1000 == 0:
             print(num_processed)
-        file_prefix = json_name[:json_name.rfind("-")+1]
+        file_prefix = json_name[:json_name.rfind("-") + 1]
         file_prefix_path = os.path.join(subdir_path, file_prefix)
         file_prefix = ntpath.basename(file_prefix)
         directory_path = subdir_path
         if directory_path.endswith("/"):
             directory_path = directory_path[:-1]
         if "/" in directory_path:
-            directory_path = directory_path[directory_path.rfind("/")+1:]
+            directory_path = directory_path[directory_path.rfind("/") + 1:]
 
         with open(json_name, 'r') as json_file:
             json_data = json.load(json_file)
-            word = open(json_name[:-13]+"word.txt","r").read().strip()
+            word = open(json_name[:-13] + "word.txt", "r").read().strip()
             for key in json_data:
                 try:
                     image_link = json_data[key]["image_link"]
@@ -39,7 +47,7 @@ for subdir in os.listdir(input_folder):
                     original_filename = json_data[key]["original_filename"]
                 except:
                     original_filename = "-"
-                correspond_file_path = directory_path +"/"+ key + ".jpg"
+                correspond_file_path = base_input_dir + "/" + directory_path + "/" + key + ".jpg"
                 index_content.append("\t".join([word, correspond_file_path, page_url, image_link]))
 
 with open(os.path.abspath(sys.argv[2]), "w") as writer:
