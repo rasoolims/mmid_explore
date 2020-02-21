669f88347f9e660abd8e9421d0a3ef895ad7c7cd
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-02-21 17:13:08 -0500

    ...

--- find_matching_passage_for_images.py
+++ find_matching_passage_for_images.py
@@ -12,14 +12,14 @@ with open(os.path.abspath(sys.argv[3]), "w", encoding="utf-8") as writer:
         if counter %100000==0:
             print(counter)
         if page_url in target_uri_dict:
-            texts = target_uri_dict[page_url].decode("utf-8", "ignore").split("\n")
+            texts = target_uri_dict[page_url].split("\n")
             contains_text = []
             for text in texts:
                 if word.lower() in text.lower():
                     contains_text.append(text)
             if len(contains_text)>0:
                 contains_text_dict = {i:text for i, text in enumerate(contains_text)}
-                image_text_dict[correspond_file_path] = {"word":word.decode("utf-8", "ignore"), "text":contains_text_dict, "url": page_url, "image":image_link}
+                image_text_dict[correspond_file_path] = {"word":word, "text":contains_text_dict, "url": page_url, "image":image_link}
     json.dump(image_text_dict, writer, ensure_ascii=False)
 
 print("done!")
