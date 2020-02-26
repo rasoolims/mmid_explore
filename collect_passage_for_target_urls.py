dafca516e34ca64f3c8a6da4bf0bbc1213d1a734
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-02-26 15:19:54 -0500

    faster writing

--- collect_passage_for_target_urls.py
+++ collect_passage_for_target_urls.py
@@ -11,15 +11,18 @@ with gzip.open(os.path.abspath(sys.argv[2]), "wt") as writer:
         if not f.endswith(".pickle.gz"):
             continue
         print(f)
+        current_output = ""
         with gzip.open(os.path.join(pickle_folder, f), "rb") as fin:
             cur_dict = pickle.load(fin)
             for target_url in cur_dict.keys():
                 try:
                     body_text = list(cur_dict[target_url]["body"].values())
                     passage_dict[target_url] = cur_dict[target_url]["title"]+"\t"+ "\t".join(body_text)
-                    writer.write(target_url + "\t" + passage_dict[target_url] + "\n")
+                    current_output.append(target_url + "\t" + passage_dict[target_url])
                 except:
                     pass
+        writer.write("\n".join(current_output))
+        writer.write("\n")
 
 print("dict length", len(passage_dict))
 print("finished")
