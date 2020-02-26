0074d5c265597fdc3fe1f6dd25f3d3393a4b1401
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-02-26 09:43:14 -0500

    convert all json in prefix path

--- convert_metadata_to_txt_for_all_files.py
+++ convert_metadata_to_txt_for_all_files.py
@@ -15,40 +15,41 @@ if base_input_dir.endswith("/"):
 if "/" in base_input_dir:
     base_input_dir = base_input_dir[base_input_dir.rfind("/") + 1:]
 
-for subdir in os.listdir(input_folder):
-    subdir_path = os.path.join(input_folder, subdir)
+for folder_matches in glob.glob(input_folder):
+    for subdir in os.listdir(folder_matches):
+        subdir_path = os.path.join(folder_matches, subdir)
 
-    for json_name in glob.glob(subdir_path + "/*metadata.json"):
-        num_processed += 1
-        if num_processed % 1000 == 0:
-            print(num_processed)
-        file_prefix = json_name[:json_name.rfind("-") + 1]
-        file_prefix_path = os.path.join(subdir_path, file_prefix)
-        file_prefix = ntpath.basename(file_prefix)
-        directory_path = subdir_path
-        if directory_path.endswith("/"):
-            directory_path = directory_path[:-1]
-        if "/" in directory_path:
-            directory_path = directory_path[directory_path.rfind("/") + 1:]
+        for json_name in glob.glob(subdir_path + "/*metadata.json"):
+            num_processed += 1
+            if num_processed % 1000 == 0:
+                print(num_processed)
+            file_prefix = json_name[:json_name.rfind("-") + 1]
+            file_prefix_path = os.path.join(subdir_path, file_prefix)
+            file_prefix = ntpath.basename(file_prefix)
+            directory_path = subdir_path
+            if directory_path.endswith("/"):
+                directory_path = directory_path[:-1]
+            if "/" in directory_path:
+                directory_path = directory_path[directory_path.rfind("/") + 1:]
 
-        with open(json_name, 'r') as json_file:
-            json_data = json.load(json_file)
-            word = open(json_name[:-13] + "word.txt", "r").read().strip()
-            for key in json_data:
-                try:
-                    image_link = json_data[key]["image_link"]
-                except:
-                    image_link = "-"
-                try:
-                    page_url = json_data[key]["google"]["ru"]
-                except:
-                    page_url = "-"
-                try:
-                    original_filename = json_data[key]["original_filename"]
-                except:
-                    original_filename = "-"
-                correspond_file_path = base_input_dir + "/" + directory_path + "/" + key + ".jpg"
-                index_content.append("\t".join([word, correspond_file_path, page_url, image_link]))
+            with open(json_name, 'r') as json_file:
+                json_data = json.load(json_file)
+                word = open(json_name[:-13] + "word.txt", "r").read().strip()
+                for key in json_data:
+                    try:
+                        image_link = json_data[key]["image_link"]
+                    except:
+                        image_link = "-"
+                    try:
+                        page_url = json_data[key]["google"]["ru"]
+                    except:
+                        page_url = "-"
+                    try:
+                        original_filename = json_data[key]["original_filename"]
+                    except:
+                        original_filename = "-"
+                    correspond_file_path = subdir_path + "/" + key + ".jpg"
+                    index_content.append("\t".join([word, correspond_file_path, page_url, image_link]))
 
 with open(os.path.abspath(sys.argv[2]), "w") as writer:
     writer.write("\n".join(index_content))
