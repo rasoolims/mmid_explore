6f4bf5ac5fb3e1700229d72b428bf3bbbf4c4d58
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-02-27 17:14:49 -0500

    adding short output

--- collect_passage_for_target_urls.py
+++ collect_passage_for_target_urls.py
@@ -18,12 +18,13 @@ print("length of url info dict", len(url_info_dict), num_url_lines)
 pickle_folder = os.path.abspath(sys.argv[2])
 
 write_count = 0
-with gzip.open(os.path.abspath(sys.argv[3]), "wt") as writer:
+with gzip.open(os.path.abspath(sys.argv[3]), "wt") as writer, gzip.open(os.path.abspath(sys.argv[4]), "wt") as short_writer:
     for f in os.listdir(pickle_folder):
         if not f.endswith(".pickle.gz"):
             continue
         print(f, write_count)
         current_output = []
+        current_short_output = []
         with gzip.open(os.path.join(pickle_folder, f), "rb") as fin:
             cur_dict = pickle.load(fin)
             for target_url in cur_dict.keys():
@@ -34,11 +35,17 @@ with gzip.open(os.path.abspath(sys.argv[3]), "wt") as writer:
                         whole_text = url_info_dict[target_url][1] +"\t"+ body_text + "\n"
                         # Doing this to make sure that the text is ok
                         t = whole_text.encode("utf-8")
+
+                        short_body = [body  for body in cur_dict[target_url]["body"].values() if url_info_dict[target_url][0].lower() in body.lower()]
+                        short_text =  "\t".join([url_info_dict[target_url][1]]+short_body)+"\n"
                         current_output.append(whole_text)
+                        current_short_output.append(short_text)
                         write_count+=1
                 except:
                     pass
         writer.write("\n".join(current_output))
         writer.write("\n")
+        short_writer.write("\n".join(current_short_output))
+        short_writer.write("\n")
 
 print("finished", write_count)
