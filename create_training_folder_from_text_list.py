8308c2a433505f6589557c3ebd946439708310e2
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-03-03 09:52:50 -0500

    writing the labels

--- create_training_folder_from_text_list.py
+++ create_training_folder_from_text_list.py
@@ -1,20 +1,20 @@
 import os, sys
 """
 Input: text file, tab-separated, first: label, second: image path, others: sentences corresponding to image
-Output: A folder with 3 subfolders: label, image, text where the last contains text files containing sentences each in
-one line
+Output: A folder with 2 subfolders and one text file: image and text folders where the last contains text files 
+containing sentences each in one line. Label file: each line contains the label for each image.
 """
 
 target_path = os.path.abspath(sys.argv[2])
 image_folder = os.path.join(target_path, "img")
-label_path = os.path.join(target_path, "label")
+label_path = os.path.join(target_path, "labels.txt")
 text_path = os.path.join(target_path, "txt")
 
 paths = [target_path, image_folder, label_path, text_path]
 for path in paths:
     if not os.path.exists(path):
         os.makedirs(path)
-
+labels = []
 image_counter = 0
 for line in open(os.path.abspath(sys.argv[1]), "r"):
     spl = line.strip().split("\t")
@@ -31,8 +31,7 @@ for line in open(os.path.abspath(sys.argv[1]), "r"):
         copy_command = " ".join(["cp", image_path, os.path.join(image_folder, img_file_name), "&"])
         os.system(copy_command)
 
-        with open(os.path.join(label_path, txt_file_name), "w") as writer:
-            writer.write(label)
+        labels.append(label)
 
         with open(os.path.join(text_path, txt_file_name), "w") as writer:
             writer.write(content)
@@ -42,5 +41,8 @@ for line in open(os.path.abspath(sys.argv[1]), "r"):
         if image_counter%1000==0:
             print(image_counter)
 
+with open(label_path, "w") as writer:
+    writer.write("\n".join(labels))
+            
 print(image_counter)
 print("finished")
