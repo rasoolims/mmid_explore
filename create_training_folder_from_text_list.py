e1fdcf64337e8eff1d5d8214bbe0784a35f10fef
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-03-04 09:24:45 -0500

    change the format of data file

--- create_training_folder_from_text_list.py
+++ create_training_folder_from_text_list.py
@@ -7,21 +7,21 @@ containing sentences each in one line. Label file: each line contains the label 
 
 target_path = os.path.abspath(sys.argv[2])
 image_folder = os.path.join(target_path, "img")
-label_path = os.path.join(target_path, "labels.txt")
-text_path = os.path.join(target_path, "txt")
+text_path = os.path.join(target_path, "index.txt")
 
-paths = [target_path, image_folder, label_path, text_path]
+paths = [target_path, image_folder]
 for path in paths:
     if not os.path.exists(path):
         os.makedirs(path)
 labels = []
 image_counter = 0
+lines = []
 for line in open(os.path.abspath(sys.argv[1]), "r"):
     spl = line.strip().split("\t")
     label = spl[0]
     image_path = spl[1]
     image_extension = image_path[image_path.rfind(".")+1:]
-    content = "\n".join(spl[2:])
+    content = "\t".join(spl[2:])
 
     img_file_name = str(image_counter) + "."+image_extension
     txt_file_name = str(image_counter) + ".txt"
@@ -30,19 +30,15 @@ for line in open(os.path.abspath(sys.argv[1]), "r"):
     else:
         copy_command = " ".join(["cp", image_path, os.path.join(image_folder, img_file_name), "&"])
         os.system(copy_command)
-
-        labels.append(label)
-
-        with open(os.path.join(text_path, txt_file_name), "w") as writer:
-            writer.write(content)
-
+        output = label +"\t"+ os.path.join(image_folder, img_file_name)+"\t"+content
+        lines.append(output)
         image_counter+=1
 
         if image_counter%1000==0:
             print(image_counter)
 
-with open(label_path, "w") as writer:
-    writer.write("\n".join(labels))
+with open(text_path, "w") as writer:
+    writer.write("\n".join(lines))
             
 print(image_counter)
 print("finished")
