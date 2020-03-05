71c8603e49acd26e459ad0ef945473f2eb16799b
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-03-05 09:30:13 -0500

    Revert "parallel copy"
    This reverts commit 87ccee7cbb3c99ec478ba62551e3507a97885f8e.

--- create_training_folder_from_text_list.py
+++ create_training_folder_from_text_list.py
@@ -16,7 +16,6 @@ for path in paths:
 labels = []
 image_counter = 0
 lines = []
-copy_commands = []
 for line in open(os.path.abspath(sys.argv[1]), "r"):
     spl = line.strip().split("\t")
     label = spl[0]
@@ -29,25 +28,14 @@ for line in open(os.path.abspath(sys.argv[1]), "r"):
     if not os.path.exists(image_path):
         print(image_path, "does not exist")
     else:
-        if len(copy_commands)==100:
-            copy_command = " ".join(["cp", image_path, os.path.join(image_folder, img_file_name)])
-            copy_commands.append(copy_command)
-            for command in copy_commands:
-                os.system(command)
-            copy_commands = []
-        else:
-            copy_command = " ".join(["cp", image_path, os.path.join(image_folder, img_file_name), "&"])
-            copy_commands.append(copy_command)
-
+        copy_command = " ".join(["cp", image_path, os.path.join(image_folder, img_file_name), "&"])
+        os.system(copy_command)
         output = label +"\t"+ os.path.join(image_folder, img_file_name)+"\t"+content
         lines.append(output)
         image_counter+=1
 
         if image_counter%1000==0:
             print(image_counter)
-
-for command in copy_commands:
-    os.system(command)
 
 with open(text_path, "w") as writer:
     writer.write("\n".join(lines))
