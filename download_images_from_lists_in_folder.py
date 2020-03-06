27ef7c167e11bbb0b037a64c3598797518218361
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-03-06 16:32:23 -0500

    accepting other types of files

--- download_images_from_lists_in_folder.py
+++ download_images_from_lists_in_folder.py
@@ -12,18 +12,22 @@ num_process = 0
 for file in os.listdir(input_folder):
     if file.endswith(".image_list.txt.gz"):
         file_number = file[:-len(".gz.pickle.gz.image_list.txt.gz")]
-        file_number = int(file_number[file_number.rfind(".")+1:])
-        new_folder = os.path.join(output_folder, str(file_number))
-        if not os.path.exists(new_folder):
-            os.makedirs(new_folder)
-        num_process+=1
+    elif file.endswith("image_list.txt.gz"):
+        file_number = file[:-len(".json.image_list.txt.gz")]
+    else:
+        continue
+    file_number = int(file_number[file_number.rfind(".")+1:])
+    new_folder = os.path.join(output_folder, str(file_number))
+    if not os.path.exists(new_folder):
+        os.makedirs(new_folder)
+    num_process+=1
 
 
-        command = ["python3", path_dir_name + "/download_images_from_list.py", os.path.join(input_folder, file), new_folder]
-        popopen = subprocess.Popen(command)
-        print(os.path.join(input_folder, file))
-        if num_process % 40 == 0:
-            print("waiting")
-            popopen.wait()
+    command = ["python3", path_dir_name + "/download_images_from_list.py", os.path.join(input_folder, file), new_folder]
+    popopen = subprocess.Popen(command)
+    print(os.path.join(input_folder, file))
+    if num_process % 40 == 0:
+        print("waiting")
+        popopen.wait()
 
 print("finished downloading all!")
