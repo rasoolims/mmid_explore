59891622f301f77eafcb2bd60fcdf7289cfa0a10
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-03-06 17:50:03 -0500

    occasional writing of labels

--- create_training_folder_from_text_list.py
+++ create_training_folder_from_text_list.py
@@ -16,31 +16,35 @@ for path in paths:
 labels = []
 image_counter = 0
 lines = []
-for line in open(os.path.abspath(sys.argv[1]), "r"):
-    spl = line.strip().split("\t")
-    label = spl[0]
-    image_path = spl[1]
-    image_extension = image_path[image_path.rfind(".")+1:]
-    content = "\t".join(spl[2:])
-
-    img_file_name = str(image_counter) + "."+image_extension
-    txt_file_name = str(image_counter) + ".txt"
-    if not os.path.exists(image_path):
-        print(image_path, "does not exist")
-    else:
-        if image_counter%100==0:
-            copy_command = " ".join(["cp", image_path, os.path.join(image_folder, img_file_name)])
-        else:
-            copy_command = " ".join(["cp", image_path, os.path.join(image_folder, img_file_name), "&"])
-        os.system(copy_command)
-        output = label +"\t"+ os.path.join(image_folder, img_file_name)+"\t"+content
-        lines.append(output)
-        image_counter+=1
-
-        if image_counter%1000==0:
-            print(image_counter)
-
 with open(text_path, "w") as writer:
+    for line in open(os.path.abspath(sys.argv[1]), "r"):
+        spl = line.strip().split("\t")
+        label = spl[0]
+        image_path = spl[1]
+        image_extension = image_path[image_path.rfind(".")+1:]
+        content = "\t".join(spl[2:])
+
+        img_file_name = str(image_counter) + "."+image_extension
+        txt_file_name = str(image_counter) + ".txt"
+        if not os.path.exists(image_path):
+            print(image_path, "does not exist")
+        else:
+            if image_counter%100==0:
+                copy_command = " ".join(["cp", image_path, os.path.join(image_folder, img_file_name)])
+            else:
+                copy_command = " ".join(["cp", image_path, os.path.join(image_folder, img_file_name), "&"])
+            os.system(copy_command)
+            output = label +"\t"+ os.path.join(image_folder, img_file_name)+"\t"+content
+            lines.append(output)
+            image_counter+=1
+
+            if image_counter%1000==0:
+                print(image_counter)
+                writer.write("\n".join(lines))
+                writer.write("\n")
+                lines = []
+
+
     writer.write("\n".join(lines))
             
 print(image_counter)
