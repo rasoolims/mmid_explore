3291827fd5076b793eefb06ecd8e5c0dc40bfaae
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-03-11 16:57:49 -0400

    download_html

---
+++ download_html_from_wiki_list.py
@@ -0,0 +1,67 @@
+import sys
+import urllib.request
+import datetime
+import time
+from functools import wraps
+import errno
+import os
+import signal
+
+class TimeoutError(Exception):
+    pass
+
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
+
+@timeout(30, "time out")
+def download_one_file(fixed_url, file_path):
+    urllib.request.urlretrieve(fixed_url, file_path)
+
+input_file = os.path.abspath(sys.argv[1])
+output_folder = os.path.abspath(sys.argv[2])
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+
+file_number = 0
+url_count = 0
+start_time = time.time()
+file_path = os.path.join(output_folder, "index.txt")
+with open(input_file) as reader:
+    for line in reader:
+        spl = line.strip().split("\t")
+        file_name = spl[0]
+        fixed_url = spl[2]
+        url_count += 1
+
+        html_file_path = os.path.join(output_folder, file_name)
+
+        try:
+            download_one_file(fixed_url, html_file_path)
+            file_number += 1
+        except:
+            pass
+
+        if url_count%100==0:
+            print(datetime.datetime.now(), url_count, file_number, time.time()-start_time)
+            start_time = time.time()
+
+    sys.stdout.write(str(url_count)+"\n")
+
+print("Written files", file_number)
+
