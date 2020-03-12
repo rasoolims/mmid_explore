05e9f0aaae5ed2a9282560822a7ad3b02d99d3f7
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-03-12 15:54:42 -0400

    fix wrong write

--- split_txt_file_to_pieces.py
+++ split_txt_file_to_pieces.py
@@ -2,16 +2,18 @@ import os, sys
 import math
 
 f = os.path.abspath(sys.argv[1])
-contents = open(f, "r").read().split("\n")
+contents = open(f, "r").read().strip().split("\n")
 
 clen = len(contents)
 split_len = math.ceil(clen/10)
 
+print(clen, split_len)
 for i in range(10):
     start = i*split_len
     end = min((i+1)*split_len, clen)
+    print(start, end)
     content_part = "\n".join(contents[start:end])
     f_name = f+"."+str(i)
     with open(f_name, "w") as writer:
-        writer.write(f_name)
+        writer.write(content_part)
         writer.write("\n")
