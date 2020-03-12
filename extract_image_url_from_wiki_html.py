4e9ae7cf648d2cab01754f2c4d141b8a3766d5e9
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-03-12 18:15:25 -0400

    image url provider per file

--- extract_image_url_from_wiki_html.py
+++ extract_image_url_from_wiki_html.py
@@ -1,8 +1,29 @@
+# coding: utf8
+
 import os, sys
 
 def retrieve_url(file_path):
     with open(file_path, "r") as reader:
         content = reader.read()
+        q1 = "fullImageLink"
+        if q1 not in content:
+            return None
+        content = content[content.find(q1)+len(q1):]
+
+        q2 = "<a href=\""
+        if q2 not in content:
+            return None
+        content = content[content.find(q2)+len(q2):]
+
+        q3 = "\"><"
+        if q3 not in content:
+            return None
+        content = content[:content.find(q3)]
+
+        if content.startswith("//"):
+            content = "https:"+content
+
+        return content
 
 
 
@@ -12,6 +33,8 @@ output_file = os.path.abspath(sys.argv[2])
 contents = []
 with open(output_file, "w") as writer:
     for f in os.listdir(input_folder):
+        if f.startswith("."):
+            continue # ignore os-specific files
         url = retrieve_url(os.path.join(input_folder, f))
         if url is not None:
             contents.append(f+"\t"+url)
