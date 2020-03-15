ca94941d1c2019bb484ec2bffe9d9ccd7d0d3066
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-03-15 11:23:41 -0400

    cat files

---
+++ cat_files_in_folder.py
@@ -0,0 +1,9 @@
+import os,sys
+
+input_folder = os.path.abspath(sys.argv[1])
+with open(os.path.abspath(sys.argv[2]), "w") as writer:
+    for f in os.listdir(input_folder):
+        with open(os.path.join(input_folder, f), "r") as reader:
+            content = reader.read().strip()
+            writer.write(content)
+            writer.write("\n")
