aeb4997c854315ad56e26a5e9ed2ac793b3dc23a
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-03-17 13:49:29 -0400

    more resize info

--- resize_images_in_folder.py
+++ resize_images_in_folder.py
@@ -7,12 +7,14 @@ from PIL import Image
 dir_path = os.path.abspath(sys.argv[1])
 new_width, new_height = int(sys.argv[2]), int(sys.argv[3])
 
+orig_sizes, new_sizes = 0, 0
 resized, all = 0, 0
 for f in os.listdir(dir_path):
     all += 1
+    file_path = os.path.join(dir_path, f)
     try:
-        file_path = os.path.join(dir_path, f)
-        im = Image.open(os.path.abspath(file_path))
+        im = Image.open(file_path)
+        orig_sizes += os.path.getsize(file_path)/(1024*1024)
         x, y = im.size
         if x * y > new_width * new_height:
             new_im = im.resize((new_width, new_height))
@@ -20,5 +22,6 @@ for f in os.listdir(dir_path):
             resized+=1
     except:
         print("problem resizing", f)
+    new_sizes += os.path.getsize(file_path) / (1024 * 1024)
 
-print("finished resizing", resized, all)
+print("finished resizing", resized, all, orig_sizes, new_sizes)
