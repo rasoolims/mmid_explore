151e82a8ee7570995a1d173635a617d0f898c61e
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-03-17 14:00:03 -0400

    fix path

--- unzip_image_gz.py
+++ unzip_image_gz.py
@@ -8,7 +8,7 @@ dir_path = os.path.abspath(sys.argv[1])
 files = os.listdir(dir_path)
 for f in files:
     if f.endswith(".gz"):
-        cur_file = os.path.join(dir_path, f[:-3])
+        cur_file = os.path.join(dir_path, f)
         orig_file = os.path.join(dir_path, f[:-3])
         if os.path.exists(orig_file):
             command = "rm "+ cur_file
