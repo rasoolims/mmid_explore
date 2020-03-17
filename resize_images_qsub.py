2d1cd7ee4dc180bb71264d9d09422d581b2971fa
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-03-17 14:07:57 -0400

    100 processes for 1000 files

--- resize_images_qsub.py
+++ resize_images_qsub.py
@@ -8,21 +8,22 @@ path_dir_name = os.path.dirname(os.path.realpath(__file__)) + "/download_wiki_im
 if not os.path.exists(config_folder):
     os.makedirs(config_folder)
 
-for folder in os.listdir(input_folder):
-    print(folder)
-    folder_path = os.path.join(input_folder, folder)
-    content = ["#$ -N " + folder]
-    content += ["#$ -o " + os.path.join(config_folder, folder + ".stdout")]
-    content += ["#$ -e " + os.path.join(config_folder, folder + ".stderr")]
+all_folders = os.listdir(input_folder)
+for i in range(0, len(all_folders), 10) :
+    folder_path = os.path.join(input_folder, all_folders[i])
+    content = ["#$ -N " + all_folders[i]]
+    content += ["#$ -o " + os.path.join(config_folder, all_folders[i] + ".stdout")]
+    content += ["#$ -e " + os.path.join(config_folder, all_folders[i] + ".stderr")]
     content += ["#$ -M rasooli@seas.upenn.edu"]
     content += ["#$ -l h_vmem=20G"]
     content += ["#$ -l mem=20G"]
     content += ["#$ -l h_rt=48:00:00"]
     content += ["#$ -cwd"]
-    command = "python3 -u " + path_dir_name  + folder_path + " 512 512"
-    content += [command]
+    for j in range(i, min(i+10, len(all_folders))):
+        content += ["python3 -u " + path_dir_name  + all_folders[j] + " 512 512"]
+
     content = "\n".join(content)
-    config_path = os.path.join(config_folder, folder) + ".sh"
+    config_path = os.path.join(config_folder, all_folders[i]) + ".sh"
     with open(config_path, "w") as writer:
         writer.write(content)
     command = "qsub " + config_path
