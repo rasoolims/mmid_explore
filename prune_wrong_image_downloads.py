b4d0297f64681f8c97313bcd75a9b81afd5d0d77
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-03-20 12:09:38 -0400

    removing pruned files

--- prune_wrong_image_downloads.py
+++ prune_wrong_image_downloads.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 
 image_url_file = os.path.abspath(sys.argv[1])
 image_list_file = os.path.abspath(sys.argv[2])
-output_file = os.path.abspath(sys.argv[3])
+image_folder = os.path.abspath(sys.argv[3])
 
 print("reading image paths")
 image_paths = defaultdict(set)
@@ -16,20 +16,24 @@ with open(image_list_file, "r") as reader:
         image_paths[file_num].add(path)
 
 print("reading output files")
-with open(output_file, "w") as writer:
-    with open(image_url_file, "r") as reader:
-        for c, line in enumerate(reader):
-            spl = line.strip().split("\t")
-            if len(spl) != 2: continue
+removed = 0
+with open(image_url_file, "r") as reader:
+    for c, line in enumerate(reader):
+        spl = line.strip().split("\t")
+        if len(spl) != 2: continue
 
-            file_num, url = spl
-            extension = url[url.rfind("."):]
-            folder = str(int(file_num)%1000)
+        file_num, url = spl
+        extension = url[url.rfind("."):]
+        folder = str(int(file_num)%1000)
 
-            for image_file in image_paths[file_num]:
-                image_extension = image_file[image_file.rfind("."):]
-                if extension != image_extension:
-                    writer.write(image_file+"\n")
+        for image_file in image_paths[file_num]:
+            image_extension = image_file[image_file.rfind("."):]
+            if extension != image_extension:
+                command = "rm " + os.path.join(image_folder, image_file) + "&"
+                os.system(command)
+                removed += 1
 
-            if (c + 1) % 1000000 == 0:
-                print(c + 1)
+        if (c + 1) % 1000000 == 0:
+            print(c + 1)
+
+print("removed files", removed)
