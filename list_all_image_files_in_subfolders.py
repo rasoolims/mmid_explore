767230bf29183658d612f616c6cd8546133fdb3b
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-03-20 11:27:12 -0400

    extrac print for finding the issue with repeated files

--- list_all_image_files_in_subfolders.py
+++ list_all_image_files_in_subfolders.py
@@ -3,9 +3,10 @@ import os,sys
 input_folder = os.path.abspath(sys.argv[1])
 output_file = os.path.abspath(sys.argv[2])
 
+file_set = set()
 with open(output_file, "w") as writer:
     for folder in os.listdir(input_folder):
-        print(folder)
+        #print(folder)
         folder_path = os.path.join(input_folder, folder)
         for file in os.listdir(folder_path):
             fl = file.lower()
@@ -15,5 +16,9 @@ with open(output_file, "w") as writer:
                 file_num = fl[:-5]
             else:
                 continue
+            if file_num not in file_set:
+                file_set.add(file_num)
+            else:
+                print("repeated", file_num)
             writer.write(file_num+"\t"+folder+"/"+file+"\n")
 print("done!")
