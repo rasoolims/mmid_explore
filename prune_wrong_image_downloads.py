269e1d752dd83b440361d05a93e0283f2ec42236
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-03-20 14:17:43 -0400

    fixed bug in pruning

--- prune_wrong_image_downloads.py
+++ prune_wrong_image_downloads.py
@@ -3,17 +3,19 @@ import sys
 from collections import defaultdict
 
 image_url_file = os.path.abspath(sys.argv[1])
-image_list_file = os.path.abspath(sys.argv[2])
-image_folder = os.path.abspath(sys.argv[3])
+image_folder = os.path.abspath(sys.argv[2])
 
 print("reading image paths")
 image_paths = defaultdict(set)
-with open(image_list_file, "r") as reader:
-    for c, line in enumerate(reader):
-        spl = line.strip().split("\t")
-        if len(spl) != 2: continue
-        file_num, path = spl
-        image_paths[file_num].add(path)
+for folder in os.listdir(image_folder):
+    print(folder)
+    folder_path = os.path.join(image_folder, folder)
+    if not os.path.isdir(folder_path): continue
+
+    for file in os.listdir(folder_path):
+        file_num = file[:file.rfind(".")]
+        file_path = os.path.join(folder_path, file)
+        image_paths[file_num].add(file_path)
 
 print("reading output files")
 removed = 0
@@ -24,18 +26,18 @@ with open(image_url_file, "r") as reader:
 
         file_num, url = spl
         extension = url[url.rfind("."):]
-        folder = str(int(file_num)%1000)
+        folder = str(int(file_num) % 1000)
 
         for image_file in image_paths[file_num]:
             image_extension = image_file[image_file.rfind("."):]
             if extension != image_extension:
                 removed += 1
                 if removed % 100 == 0:
-                    command = "rm " + os.path.join(image_folder, image_file)
+                    command = "rm " + image_file
                 else:
-                    command = "rm " + os.path.join(image_folder, image_file) + " &"
+                    command = "rm " + image_file + " &"
                 print(command)
-                os.system(command)
+                # os.system(command)
 
         if (c + 1) % 1000000 == 0:
             print(c + 1)
