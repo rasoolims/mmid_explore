473beb3fe7ca88afad5edb02041186ff146126a6
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-03-20 12:03:58 -0400

    to_prune based on file list (much faster)

--- prune_wrong_image_downloads.py
+++ prune_wrong_image_downloads.py
@@ -1,11 +1,21 @@
 import os
 import sys
-import glob
+from collections import defaultdict
 
 image_url_file = os.path.abspath(sys.argv[1])
-image_folder = os.path.abspath(sys.argv[2])
+image_list_file = os.path.abspath(sys.argv[2])
 output_file = os.path.abspath(sys.argv[3])
 
+print("reading image paths")
+image_paths = defaultdict(set)
+with open(image_list_file, "r") as reader:
+    for c, line in enumerate(reader):
+        spl = line.strip().split("\t")
+        if len(spl) != 2: continue
+        file_num, path = spl
+        image_paths[file_num].add(path)
+
+print("reading output files")
 with open(output_file, "w") as writer:
     with open(image_url_file, "r") as reader:
         for c, line in enumerate(reader):
@@ -15,11 +25,11 @@ with open(output_file, "w") as writer:
             file_num, url = spl
             extension = url[url.rfind("."):]
             folder = str(int(file_num)%1000)
-            image_files = glob.glob(os.path.join(image_folder, folder, file_num+"*"))
-            for image_file in image_files:
+
+            for image_file in image_paths[file_num]:
                 image_extension = image_file[image_file.rfind("."):]
                 if extension != image_extension:
-                    writer.write(os.path.join(image_folder, folder, image_file)+"\n")
+                    writer.write(image_file+"\n")
 
-            if (c + 1) % 10000 == 0:
+            if (c + 1) % 1000000 == 0:
                 print(c + 1)
