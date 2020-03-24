e9b48e7ecf06c84df24a73921155c09758923d5d
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-03-24 10:59:18 -0400

    running the actual command

--- collect_wiki_text_info_qsub.py
+++ collect_wiki_text_info_qsub.py
@@ -36,6 +36,6 @@ for folder in os.listdir(dump_folder):
         writer.write(content)
     command = "qsub " + config_path
     print(command)
-    # os.system(command)
+    os.system(command)
 
 print("Done!")
