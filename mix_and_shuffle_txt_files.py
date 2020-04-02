fa528a41b068b07905c00867db29918eff934eef
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-04-02 13:58:23 -0400

    sum_sizes

--- mix_and_shuffle_txt_files.py
+++ mix_and_shuffle_txt_files.py
@@ -9,5 +9,7 @@ for file in os.listdir(input_folder):
     file_path = os.path.join(input_folder, file)
     file_size = os.path.getsize(file_path)
     file_sizes[file_path] = file_size
+sum_sizes = sum(file_sizes.values())
 file_sizes = sorted(file_sizes.items(), key=lambda x: x[1], reverse=True)
 print(file_sizes)
+print(sum_sizes)
