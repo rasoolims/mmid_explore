95f4913e8a2c6ec6988bff2b03f615f33443d836
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-04-02 12:29:34 -0400

    resize while finding corrupt

--- find_corrupt_images.py
+++ find_corrupt_images.py
@@ -16,12 +16,19 @@ transform = transforms.Compose([  # [1]
         std=[0.229, 0.224, 0.225]  # [7]
     )])
 
+# Also resize them if possible
+Image.MAX_IMAGE_PIXELS = None
+
 input_folder = os.path.abspath(sys.argv[1])
 for file in os.listdir(input_folder):
     if file.lower().endswith(".jpg") or file.lower().endswith(".jpeg") or file.lower().endswith(".png"):
         path = os.path.join(input_folder, file)
         try:
             image = Image.open(path).convert("RGB")
+            x, y = image.size
+            if x * y > 1024 * 1024:
+                new_im = image.resize((1024, 1024))
+                new_im.save(path)
             image = transform(image)
         except:
             print(path)
