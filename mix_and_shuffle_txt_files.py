166094c6c643644cb49b069598f76b95f527cc1f
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-04-02 14:08:50 -0400

    print share numbers

--- mix_and_shuffle_txt_files.py
+++ mix_and_shuffle_txt_files.py
@@ -10,15 +10,16 @@ for file in os.listdir(input_folder):
     file_size = os.path.getsize(file_path)
     file_sizes[file_path] = file_size
 
-file_handles = {f:open(f, "r") for f in file_sizes.keys()}
+file_handles = {f: open(f, "r") for f in file_sizes.keys()}
 sum_sizes = sum(file_sizes.values())
 
 random_share = []
 for path in file_sizes.keys():
-    share = max(1, int(file_sizes[path]/(1024*1024)))
-    random_share += [path]*share
+    share = max(1, int(file_sizes[path] / (1024 * 1024)))
+    print(path, share)
+    random_share += [path] * share
 
-print(random_share)
+print(len(random_share))
 
 for f in file_handles:
     file_handles[f].close()
