e316a4b83f692d33694d54a8f676048a4138f7fd
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-04-02 14:02:46 -0400

    adding file handles

--- mix_and_shuffle_txt_files.py
+++ mix_and_shuffle_txt_files.py
@@ -9,7 +9,12 @@ for file in os.listdir(input_folder):
     file_path = os.path.join(input_folder, file)
     file_size = os.path.getsize(file_path)
     file_sizes[file_path] = file_size
+    
+file_handles = {f:open(f, "r") for f in file_sizes.keys()}
 sum_sizes = sum(file_sizes.values())
 file_sizes = sorted(file_sizes.items(), key=lambda x: x[1], reverse=True)
 print(file_sizes)
 print(sum_sizes)
+
+for f in file_handles:
+    file_handles[f].close()
