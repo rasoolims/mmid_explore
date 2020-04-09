5be93731b96476a31996edc1f8a3afa3a877ee6c
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-04-09 15:25:14 -0400

    sublangs

--- mix_all_image_captions.py
+++ mix_all_image_captions.py
@@ -4,9 +4,18 @@ from collections import defaultdict
 
 input_folder = os.path.abspath(sys.argv[1])
 output_file = os.path.abspath(sys.argv[2])
+use_all_langs = True
+sub_langs = None
+
+if len(sys.argv)>3:
+    sub_langs = set(sys.argv[3].strip().split(","))
+    use_all_langs = False
 
 image_dict = defaultdict(list)
 for file in os.listdir(input_folder):
+    if not use_all_langs:
+        if file[:-4] not in sub_langs:
+            continue
     print(file)
     with open(os.path.join(input_folder, file), "r") as fp:
         for line in fp:
