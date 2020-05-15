40996e786341a786c9193d1f318d08720e93a8db
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-05-15 16:40:19 -0400

    remove caption -- fixed bug

--- fix_captions.py
+++ fix_captions.py
@@ -10,7 +10,7 @@ caption_output_file = os.path.abspath(sys.argv[2])
 saved, skipped = 0, 0
 with open(caption_file, "r") as r, open(caption_output_file, "w") as w:
     for line in r:
-        path, lang, caption = line.strip().split("\t")
+        path, caption = line.strip().split("\t")
         if "=" in caption or caption == "thumb" or caption == "left" or caption == "right" or caption == "thumbnail":
             skipped += 1
             continue
