6cbe48514d78ea740c07d0a59135c5587f5029bd
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-05-15 17:06:23 -0400

    fix captions for json files

--- fix_image_doc_captions.py
+++ fix_image_doc_captions.py
@@ -12,18 +12,16 @@ saved, skipped = 0, 0
 with open(caption_file, "rb") as r:
     doc_dicts = json.load(r)
     for d_num, doc in enumerate(doc_dicts):
-        to_remove = []
+        new_list = []
         for image in doc["images"]:
             caption = image["caption"]
             if "=" in caption or caption == "thumb" or caption == "left" or caption == "right" or caption == "thumbnail":
-                to_remove.append(image)
                 skipped += 1
                 continue
             else:
+                new_list.append(image)
                 saved += 1
-
-        for im in to_remove:
-            del doc["images"][im]
+        doc["images"] = new_list
 
     with open(caption_output_file, "wb") as w:
         json.dump(doc_dicts, w)
